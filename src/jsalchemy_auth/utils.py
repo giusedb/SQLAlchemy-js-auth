707fc240d7245bf1f750b57a6e3c49867b015707
Giuseppe Di Bona <giusedb@gmail.com>
2025-09-06 11:52:54 +0200

    Define `ContextSet` for traverse efficiency

--- src/jsalchemy_auth/utils.py
+++ src/jsalchemy_auth/utils.py
@@ -12,9 +12,80 @@ class Context(NamedTuple):
     table: str
     id: int
 
+    def __add__(self, other):
+        if isinstance(other, ContextSet):
+            return ContextSet(self.table, (self.id,) + other.ids)
+        if isinstance(other, Context):
+            return ContextSet(self.table, (self.id, other.id))
+        return ContextSet(self.table, (self.id, other))
+
+class ContextSet(NamedTuple):
+    table: str
+    ids: tuple[int]
+
+    class ContextSetIterator:
+
+        def __init__(self, table, ids):
+            self.table = table
+            self.ids = ids
+            self.index = -1
+            self.length = len(ids) - 1
+
+        def __next__(self):
+            if self.index < self.length:
+                self.index += 1
+                return Context(self.table, self.ids[self.index])
+            raise StopIteration
+
+    def __bool__(self):
+        return bool(self.ids)
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __iter__(self):
+        return iter((Context(self.table, id) for id in self.ids))
+
+    def __add__(self, other):
+        if isinstance(other, ContextSet):
+            if self.table != other.table:
+                raise ValueError("ContextSet tables must match")
+            return ContextSet(self.table, self.ids + other.ids)
+        if isinstance(other, Context):
+            if self.table != other.table:
+                raise ValueError("ContextSet tables must match")
+            return ContextSet(self.table, self.ids + [other.id])
+        return ContextSet(self.table, self.ids + [other])
+
+    def __iter__(self):
+        return self.ContextSetIterator(self.table, self.ids)
+
+    def __contains__(self, item):
+        if isinstance(item, Context):
+            if item.table != self.table:
+                return False
+            return item.id in self.ids
+        return item in self.ids
+
+    @staticmethod
+    def join(*contexts):
+        if not contexts:
+            raise ValueError("ContextSet.join requires at least one context")
+        if len({c.table for c in contexts}) != 1:
+            raise ValueError("ContextSet.join requires contexts with the same table")
+
+        ids = set()
+        for context in contexts:
+            if isinstance(context, ContextSet):
+                ids.update(context.ids)
+            elif isinstance(context, Context):
+                ids.add(context.id)
+        return ContextSet(contexts[0].table, tuple(ids))
+
+
 def to_context(object: DeclarativeBase) -> Context:
     """Convert a DeclarativeBase object to a Context."""
-    if isinstance(object, Context):
+    if isinstance(object, (Context, ContextSet)):
         return object
     return Context(object.__tablename__, object.id)
 
